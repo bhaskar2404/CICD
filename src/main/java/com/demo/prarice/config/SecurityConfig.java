6f708ebd08dbb393f816d225db7c0b717111c993
bhaskarvanam <--global>
2026-07-04 23:12:34 -0400

    initial Spring  boot project

---
+++ src/main/java/com/demo/prarice/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.demo.prarice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()  // allow actuator
+                        .requestMatchers("/api/v1/entry").permitAll()                  // allow your API
+                        .anyRequest().authenticated()                                   // other endpoints require auth
+                )
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
---
+++ src/main/java/com/demo/prarice/controller/DemoController.java
@@ -0,0 +1,25 @@
+package com.demo.prarice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/entry")
+public class DemoController {
+
+    @GetMapping
+    public ResponseEntity<Map> getWelcome(){
+        Map<String,String> envValues=new HashMap<>();
+
+
+        envValues.put("HostName",System.getenv("HOSTNAME"));
+        envValues.put("Pod Namespace",System.getenv("POD_NAMESPACE"));
+        envValues.put("Node Name",System.getenv("NODE_NAME"));
+        return ResponseEntity.ok(envValues);
+    }
+}
