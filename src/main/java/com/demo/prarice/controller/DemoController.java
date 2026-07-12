a384dd7544c10e6244bbd472cefffbbcfe7593ef
bhaskarvanam <--global>
2026-07-12 11:00:33 -0400

    added kubernetes commands added

--- src/main/java/com/demo/prarice/controller/DemoController.java
+++ src/main/java/com/demo/prarice/controller/DemoController.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 @RestController
@@ -20,6 +23,9 @@ public class DemoController {
         envValues.put("HostName",System.getenv("HOSTNAME"));
         envValues.put("Pod Namespace",System.getenv("POD_NAMESPACE"));
         envValues.put("Node Name",System.getenv("NODE_NAME"));
+        ZonedDateTime zoneDateTimeNow = ZonedDateTime.now();
+
+        envValues.put("Date",zoneDateTimeNow.toString() );
         return ResponseEntity.ok(envValues);
     }
 }
